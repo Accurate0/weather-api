f0640f9990ba4107958aaf37a9ffadb73dda2264
Anurag Singh <contact@anurag.sh>
2022-03-11 22:20:09 +0800

    log sensitive info to test :D

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -8,15 +8,19 @@ namespace WeatherApi.Controllers;
 public class WeatherController : ControllerBase
 {
     private readonly ILogger<WeatherController> _logger;
+    private IConfiguration _configuration;
 
-    public WeatherController(ILogger<WeatherController> logger)
+    public WeatherController(ILogger<WeatherController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     [HttpGet("Current")]
     public Weather Get()
     {
+        // log sensitive information :D
+        _logger.LogInformation(_configuration.GetConnectionString("Database"));
         _logger.LogInformation("request for current weather");
         return new Weather();
     }
