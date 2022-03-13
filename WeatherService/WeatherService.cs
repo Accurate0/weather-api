b9cecb1e9171a18c154baaa69af87fcd929765d0
Anurag Singh <contact@anurag.sh>
2022-03-14 00:26:11 +0800

    fa

---
+++ WeatherService/WeatherService.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Logging;
+using LibWeather;
+using Microsoft.Extensions.Configuration;
+
+namespace WeatherService
+{
+    public class WeatherService
+    {
+        [FunctionName("WeatherService")]
+        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            log.LogCritical(config.GetConnectionString("Database"));
+        }
+    }
+}
