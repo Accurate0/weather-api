2d654721275e14397087a3aa686f8ad256453b2a
Anurag Singh <contact@anurag.sh>
2022-03-15 20:10:41 +0800

    add deploy2

---
+++ WeatherService2/Function.cs
@@ -0,0 +1,21 @@
+using Amazon.Lambda.Core;
+using Newtonsoft.Json.Linq;
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace WeatherService2;
+
+public class Function
+{
+
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string FunctionHandler(JObject input, ILambdaContext context)
+    {
+        return "test";
+    }
+}
