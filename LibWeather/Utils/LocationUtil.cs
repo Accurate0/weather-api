ac763969f96fdc15259fd536c87fb563ed7cbc49
Anurag Singh <contact@anurag.sh>
2022-03-25 20:43:20 +0800

    data over branching

--- LibWeather/Utils/LocationUtil.cs
+++ LibWeather/Utils/LocationUtil.cs
@@ -4,33 +4,24 @@ namespace LibWeather.Utils
 {
     public class LocationUtil
     {
+        public static Dictionary<int, Location> WmoLocationMap = new()
+        {
+            [94608] = Location.Perth,
+            [94610] = Location.PerthAirport,
+            [94602] = Location.RottnestIsland,
+            [95936] = Location.MelbourneOlympicPark,
+            [94970] = Location.Hobart,
+            [94648] = Location.AdelaideWestTerrace,
+            [94576] = Location.Brisbane,
+            [94120] = Location.DarwinAirport,
+            [94768] = Location.SydneyObservatoryHill,
+            [94926] = Location.Canberra,
+        };
         public static Location GetLocationFromWmoId(int wmo)
         {
-            switch (wmo)
-            {
-                case 94608:
-                    return Location.Perth;
-                case 94610:
-                    return Location.PerthAirport;
-                case 94602:
-                    return Location.RottnestIsland;
-                case 95936:
-                    return Location.MelbourneOlympicPark;
-                case 94970:
-                    return Location.Hobart;
-                case 94648:
-                    return Location.AdelaideWestTerrace;
-                case 94576:
-                    return Location.Brisbane;
-                case 94120:
-                    return Location.DarwinAirport;
-                case 94768:
-                    return Location.SydneyObservatoryHill;
-                case 94926:
-                    return Location.Canberra;
-                default:
-                    return Location.Unknown;
-            }
+            Location location;
+            WmoLocationMap.TryGetValue(wmo, out location);
+            return location;
         }
 
 
